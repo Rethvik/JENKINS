eca2ccea60ea4c90fc6a6068dc174ae54254ab10
Rethvik <rethvikdoddapaneni3@gmail.com>
2025-07-12 11:42:41 +0530

    printing sys

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 import sys
-if(len(sys.argv)>1):
-    print("Now I will say based on your age")
-    if(int(sys.argv[0])>20):
-        print('You entered yours 20, carefull!!!')
-    else:
-        print("Enjoy the life")
-else:
-    print("Live on your life")
+# if(len(sys.argv)>1):
+#     print("Now I will say based on your age")
+#     if(int(sys.argv[0])>20):
+#         print('You entered yours 20, carefull!!!')
+#     else:
+#         print("Enjoy the life")
+# else:
+#     print("Live on your life")
+print(sys.argv)
